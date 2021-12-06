187b389035456c05199d7c63364cffdf8ab9cc89
webprogramming1 <webprogramming1@naver.com>
2021-12-06 11:48:14 +0900

    1206 수정

--- src/main/java/com/mozzle/web/ctrl/manage/ManageCtrl.java
+++ src/main/java/com/mozzle/web/ctrl/manage/ManageCtrl.java
@@ -1,5 +1,23 @@
 package com.mozzle.web.ctrl.manage;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 public class ManageCtrl {
 	
 	@RequestMapping(value = "/resiterMozzle", method = RequestMethod.POST)
@@ -27,8 +45,12 @@ public class ManageCtrl {
 
 			int mediumHeight = 550;				
 			int mediumWidth = (int)(resizeRatio * mediumHeight);
-
-			BufferedImage thumbnail_medium = Thumbnails.of(image).size(mediumWidth,mediumHeight).asBufferedImage();
+			
+			//dependency 추가
+			/*
+			 * BufferedImage thumbnail_medium =
+			 * Thumbnails.of(image).size(mediumWidth,mediumHeight).asBufferedImage();
+			 */
 
 			//DB 연결 후 
 			//S3Util_Thumbs.uploadImageToS3(thumbnail_medium, resizeFileName_medium);
