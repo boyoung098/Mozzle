e8db57388ab93894c56cb3b8bdf030124838ef83
uoouug <uoouug@gmail.com>
2021-12-09 09:19:44 +0900

    작성자 : 정민정 게시판, 북마크, 신고 관련 sql작성

--- src/main/java/com/mozzle/web/service/board/IBoardService.java
+++ src/main/java/com/mozzle/web/service/board/IBoardService.java
@@ -1,5 +1,21 @@
 package com.mozzle.web.service.board;
 
-public interface IBoardService {
+import java.util.List;
 
+import com.mozzle.web.dto.board.Board;
+
+public interface IBoardService {
+	public int insertBoard(Board board);
+	public int updateBoard(Board board);
+	public int deleteBoard(int num);
+	public List<Board> selectOneBoard(Board board);
+	
+	public int bookmarkBoard(Board board);
+	public int bookmarkDelete(Board board);
+	
+	public int replyIn(Board board);
+	public int replyUp(Board board);
+	public int replyDl(Board board);
+	
+	public int reportBoard(Board board);
 }
---
+++ src/main/java/com/mozzle/web/service/board/IBoardServiceImpl.java
@@ -0,0 +1,69 @@
+package com.mozzle.web.service.board;
+
+import java.util.List;
+
+import com.mozzle.web.dto.board.Board;
+
+public class IBoardServiceImpl implements IBoardService {
+
+	@Override
+	public int insertBoard(Board board) {
+		
+		return 0;
+	}
+
+	@Override
+	public int updateBoard(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteBoard(int num) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Board> selectOneBoard(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int bookmarkBoard(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int bookmarkDelete(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int replyIn(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int replyUp(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int replyDl(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int reportBoard(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
