c2b1477626dba076855c97d5f18795627bb4e43e
hyunkyungLim <lhk900810@naver.com>
2021-12-09 09:22:04 +0900

    작성자: 임현경 날 짜: 12/9 내 용: manage 컨트롤러 및 흐름제어

--- src/main/java/com/mozzle/web/ctrl/manage/ManageCtrl.java
+++ src/main/java/com/mozzle/web/ctrl/manage/ManageCtrl.java
@@ -1,52 +1,64 @@
 package com.mozzle.web.ctrl.manage;
 
-
+import java.io.File;
+import java.util.Iterator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import com.mozzle.web.dto.manage.ManageDto;
+import com.mozzle.web.dto.manage.MozzleDto;
 
 @Controller
+@RequestMapping("/{manage}/")
 public class ManageCtrl {
 	
-	@RequestMapping(value = "/registerMozzle.do", method = RequestMethod.GET)
-	public String resiterMozzle(ManageDto mDto) {
-		
-		
-		return "registerMozzle";
+	@RequestMapping(value = "/registMozzleForm.do", method = RequestMethod.GET)
+	public String registMozzleForm() {
+
+		return "manage/registMozzleForm";
 	}
 	
-	@RequestMapping(value = "/imageUpload.do", method = RequestMethod.POST)
-	public String imageUpload() {
+	// ajax에서 호출
+	@RequestMapping(value = "/imageUpload/post")
+	@ResponseBody
+	public String imageUpload(MultipartHttpServletRequest multipartHttpServletRequest) {
 		
-		/*
-		 * String realPath = request.getServletContext().getRealPath("image"); int id;
-		 * String fileName = null; String contextPath =
-		 * request.getServletContext().getContextPath(); String userProfile = null;
-		 * 
-		 * try { MultipartRequest multi = new MultipartRequest( request, realPath,
-		 * 1024*1024*2, "UTF-8", new DefaultFileRenamePolicy()); fileName =
-		 * multi.getFilesystemName("userProfile"); System.out.println("fileName : " +
-		 * fileName);
-		 * 
-		 * id = Integer.parseInt(multi.getParameter("id"));
-		 * 
-		 * userProfile = contextPath + "/image/" + fileName; UsersRepository
-		 * usersRepository = UsersRepository.getInstance(); int result =
-		 * usersRepository.update(id, userProfile);
-		 * 
-		 * if (result == 1) { HttpSession session = request.getSession(); Users
-		 * principal = usersRepository.findById(id); session.setAttribute("principal",
-		 * principal);
-		 * 
-		 * Script.href("사진 변경완료","/blog/index.jsp" , response); } else {
-		 * Script.back("사진변경실패", response); } } catch (Exception e) {
-		 * e.printStackTrace(); Script.getMessage("요류 : " + e.getMessage() , response);
-		 * }
-		 */
+		Iterator<String> itr = multipartHttpServletRequest.getFileNames();
+		
+		String filePath = "<%=request.getContextPath()%>/cover";
+		
+		while (itr.hasNext()) {
+			
+			MultipartFile image = multipartHttpServletRequest.getFile(itr.next());
+			
+			// 원 파일명
+			String image_orgin = image.getOriginalFilename();
+			String image_saved = filePath+"/"+image_orgin; 
+			
+			 try {
+	                image.transferTo(new File(image_saved)); //파일저장 실제로는 service에서 처리
+	                
+	                System.out.println("image_orgin => "+image_orgin);
+	                System.out.println("image_saved => "+image_saved);
+	     
+	            } catch (Exception e) {
+	                System.out.println("ERROR======>"+image_saved);
+	                e.printStackTrace();
+	            }
+		}
+		
+		return "success";
+	}
+	
+	@RequestMapping(value = "/registMozzle.do", method = RequestMethod.POST)
+	public String registMozzle(MozzleDto mozzle) {
+		
+		
 		return null;
-	   }
+	}
 
 }
